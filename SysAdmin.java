8392f732fe73d62a2d320b31796989c18ab002ed
TheCatism <dzsaraka@gmail.com>
2017-11-28 20:50:02 -0500

    Update SysAdmin.java

--- SysAdmin.java
+++ SysAdmin.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,12 +18,23 @@ public class SysAdmin extends WhManager {
     private String user;
     private String email;
     private String pass;
-
+     private ArrayList<Account> a;
     public SysAdmin(String f, String l, String e, String u, String p) {
         super(f, l, e, u, p);
+        a= new ArrayList<Account>();
+        
     }
-
-    public void delete() {
+    public void add(Account b){
+        a.add(b);
+    }
+    public void delete(String b) {
+        for (Account ab : a) {
+          if (ab.getUser().equals(b)) {
+               System.out.println(ab.getName());
+               ab.remove();
+            }
+        }
+        
     }
 
     public void edit() {
