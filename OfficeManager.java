0dbb2789a03dc9f194fa191dffcfb786d6bd0a1e
Ivannator <31941571+Ivannator@users.noreply.github.com>
2017-12-03 21:40:32 -0500

    Update OfficeManager.java

--- OfficeManager.java
+++ OfficeManager.java
@@ -8,8 +8,10 @@
  *
  * @author Denzel Saraka
  */
-public class OfficeManager extends SalesAssociate {
+public class OfficeManager extends WhManager {
     private ArrayList<SalesAssociates> sA = new ArrayList<>();
+    private WhManager WhManagerArray[1];
+    private ArrayList<Double> commision = new ArrayList<>();
     private String first;
     private String last;
     private String user;
@@ -32,6 +34,7 @@ public class OfficeManager extends SalesAssociate {
         for(int i = 0 ; i < sA.size(); i++){
             salAs = sA.get(i);
             ArrayList tr = salAs.getTransactions();
+            salesCommision(tr);
             name = salAs.getUsername();
             bw.write(name);
             bw.newLine();
@@ -45,11 +48,27 @@ public class OfficeManager extends SalesAssociate {
     }
         
     public void orderNewParts(){
-
+        ArrayList parts = WhManager[0].getWarehouse().getArrayList();
+        int a =0;
+        for(int i = 0; i < parts.size(); i++){
+            if(parts.get(i).getQuantity() < 5)
+                //*TextArea*.append("You need to order more " + parts.get(i).getIName() + "!");
+            else
+                a++;
+        }
+        if(a == parts.size())
+             //*TextArea*.append("You are good on all products!");
+        else
+             //*TextArea*.append("All other products are good!");
     }
         
-    public void salesCommission(){
-
+    public void salesCommission(ArrayList t){
+        double total = 0.0;
+        for(int i = 0; i < t.size(); i++){
+            total += t.get(i).getPrice();
+        }
+        double com = total * .15;
+        commision.add(com);
     }
 
 }
